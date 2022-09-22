63c4edec979a4dd6139ea8208edf50a5a0cfd99f
muhta <muhtasim.nabi@gmail.com>
2022-09-21 19:20:32 -0500

    updated how recipes how visualized using JEI

--- src/main/java/com/newjumper/spacedustry/integration/ConstructingCategory.java
+++ src/main/java/com/newjumper/spacedustry/integration/ConstructingCategory.java
@@ -28,16 +28,23 @@ public class ConstructingCategory implements IRecipeCategory<ConstructingRecipe>
 
     private final IDrawable background;
     private final IDrawable icon;
-    private final LoadingCache<Integer, IDrawableAnimated> cachedArrows;
+    private final LoadingCache<Integer, IDrawableAnimated> progressArrow;
+    private final LoadingCache<Integer, IDrawableAnimated> fuelArrow;
 
     public ConstructingCategory(IGuiHelper guiHelper) {
         this.background = guiHelper.createDrawable(TEXTURE, 63, 16, 94, 54);
         this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(SpacedustryBlocks.CONSTRUCTOR.get()));
 
-        this.cachedArrows = CacheBuilder.newBuilder().maximumSize(25).build(new CacheLoader<>() {
+        this.progressArrow = CacheBuilder.newBuilder().maximumSize(25).build(new CacheLoader<>() {
             @Override
             public IDrawableAnimated load(Integer time) {
                 return guiHelper.drawableBuilder(TEXTURE, 176, 14, 24, 17).buildAnimated(time, IDrawableAnimated.StartDirection.LEFT, false);
+            }
+        });
+        this.fuelArrow = CacheBuilder.newBuilder().maximumSize(15).build(new CacheLoader<>() {
+            @Override
+            public IDrawableAnimated load(Integer time) {
+                return guiHelper.drawableBuilder(TEXTURE, 176, 0, 14, 14).buildAnimated(time, IDrawableAnimated.StartDirection.TOP, true);
             }
         });
     }
@@ -71,10 +78,11 @@ public class ConstructingCategory implements IRecipeCategory<ConstructingRecipe>
 
     @Override
     public void draw(ConstructingRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack stack, double mouseX, double mouseY) {
-        this.cachedArrows.getUnchecked(100).draw(stack, 36, 18);
+        this.progressArrow.getUnchecked(100).draw(stack, 36, 18);
+        this.fuelArrow.getUnchecked(220).draw(stack, 13, 20);
 
         Font fontRenderer = Minecraft.getInstance().font;
-        int stringWidth = fontRenderer.width((10) + "s");
-        fontRenderer.draw(stack, (10) + "s", background.getWidth() - stringWidth, 45, 0xff808080);
+        int stringWidth = fontRenderer.width("10s");
+        fontRenderer.draw(stack, "10s", background.getWidth() - stringWidth, 45, 0xff808080);
     }
 }
