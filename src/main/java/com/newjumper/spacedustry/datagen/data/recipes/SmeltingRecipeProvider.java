e1f464bdab9b4d918ecc879017dce9daa500f941
muhta <muhtasim.nabi@gmail.com>
2022-10-19 19:09:40 -0500

    fixed recipe locations

--- src/main/java/com/newjumper/spacedustry/datagen/data/recipes/SmeltingRecipeProvider.java
+++ src/main/java/com/newjumper/spacedustry/datagen/data/recipes/SmeltingRecipeProvider.java
@@ -1,17 +1,20 @@
 package com.newjumper.spacedustry.datagen.data.recipes;
 
 import com.google.common.collect.ImmutableList;
+import com.newjumper.spacedustry.Spacedustry;
 import com.newjumper.spacedustry.block.SpacedustryBlocks;
 import com.newjumper.spacedustry.item.SpacedustryItems;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class SmeltingRecipeProvider extends RecipeProvider implements IConditionBuilder {
@@ -30,19 +33,27 @@ public class SmeltingRecipeProvider extends RecipeProvider implements ICondition
     protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
         SimpleCookingRecipeBuilder.smelting(Ingredient.of(SpacedustryBlocks.COBBLED_MOON_STONE.get()), SpacedustryBlocks.MOON_STONE.get().asItem(), 0.2f, 200).unlockedBy("has_cobbled_moon_stone", has(SpacedustryBlocks.COBBLED_MOON_STONE.get())).save(consumer);
 
-        oreSmelting(consumer, ALUMINUM_ORES, SpacedustryItems.ALUMINUM_INGOT.get(), 2.8f, 200, "aluminum_ingot");
-        oreSmelting(consumer, LITHIUM_ORES, SpacedustryItems.LITHIUM_INGOT.get(), 3f, 200, "lithium_ingot");
-        oreSmelting(consumer, MOLYBDENITE_ORES, SpacedustryItems.MOLYBDENUM_INGOT.get(), 3f, 200, "molybdenum_ingot");
-        oreSmelting(consumer, NICKEL_ORES, SpacedustryItems.NICKEL_INGOT.get(), 3f, 200, "nickel_ingot");
-        oreSmelting(consumer, SILICON_ORES, SpacedustryItems.SILICON.get(), 3f, 200, "silicon");
-        oreSmelting(consumer, TUNGSTEN_ORES, SpacedustryItems.TUNGSTEN_INGOT.get(), 4f, 200, "tungsten_ingot");
+        oreSmelting(ALUMINUM_ORES, SpacedustryItems.ALUMINUM_INGOT.get(), 2.8f, "aluminum_ingot", consumer);
+        oreSmelting(LITHIUM_ORES, SpacedustryItems.LITHIUM_INGOT.get(), 3f, "lithium_ingot", consumer);
+        oreSmelting(MOLYBDENITE_ORES, SpacedustryItems.MOLYBDENUM_INGOT.get(), 3f, "molybdenum_ingot", consumer);
+        oreSmelting(NICKEL_ORES, SpacedustryItems.NICKEL_INGOT.get(), 3f, "nickel_ingot", consumer);
+        oreSmelting(SILICON_ORES, SpacedustryItems.SILICON.get(), 3f, "silicon", consumer);
+        oreSmelting(TUNGSTEN_ORES, SpacedustryItems.TUNGSTEN_INGOT.get(), 4f, "tungsten_ingot", consumer);
 
-        oreBlasting(consumer, ALUMINUM_ORES, SpacedustryItems.ALUMINUM_INGOT.get(), 2.8f, 100, "aluminum_ingot");
-        oreBlasting(consumer, LITHIUM_ORES, SpacedustryItems.LITHIUM_INGOT.get(), 3f, 100, "lithium_ingot");
-        oreBlasting(consumer, MOLYBDENITE_ORES, SpacedustryItems.MOLYBDENUM_INGOT.get(), 3f, 100, "molybdenum_ingot");
-        oreBlasting(consumer, NICKEL_ORES, SpacedustryItems.NICKEL_INGOT.get(), 3f, 100, "nickel_ingot");
-        oreBlasting(consumer, SILICON_ORES, SpacedustryItems.SILICON.get(), 3f, 100, "silicon");
-        oreBlasting(consumer, TUNGSTEN_ORES, SpacedustryItems.TUNGSTEN_INGOT.get(), 4f, 100, "tungsten_ingot");
+        oreBlasting(ALUMINUM_ORES, SpacedustryItems.ALUMINUM_INGOT.get(), 2.8f, "aluminum_ingot", consumer);
+        oreBlasting(LITHIUM_ORES, SpacedustryItems.LITHIUM_INGOT.get(), 3f, "lithium_ingot", consumer);
+        oreBlasting(MOLYBDENITE_ORES, SpacedustryItems.MOLYBDENUM_INGOT.get(), 3f, "molybdenum_ingot", consumer);
+        oreBlasting(NICKEL_ORES, SpacedustryItems.NICKEL_INGOT.get(), 3f, "nickel_ingot", consumer);
+        oreBlasting(SILICON_ORES, SpacedustryItems.SILICON.get(), 3f, "silicon", consumer);
+        oreBlasting(TUNGSTEN_ORES, SpacedustryItems.TUNGSTEN_INGOT.get(), 4f, "tungsten_ingot", consumer);
+    }
+
+    private void oreSmelting(List<ItemLike> ingredients, ItemLike result, float experience, String group, Consumer<FinishedRecipe> consumer) {
+        for(ItemLike item : ingredients) SimpleCookingRecipeBuilder.smelting(Ingredient.of(item), result, experience, 200).group(group).unlockedBy(getHasName(item), has(item)).save(consumer, new ResourceLocation(Spacedustry.MOD_ID, getItemName(result) + "_from_smelting_" + getItemName(item)));
+    }
+
+    private void oreBlasting(List<ItemLike> ingredients, ItemLike result, float experience, String group, Consumer<FinishedRecipe> consumer) {
+        for(ItemLike item : ingredients) SimpleCookingRecipeBuilder.blasting(Ingredient.of(item), result, experience, 100).group(group).unlockedBy(getHasName(item), has(item)).save(consumer, new ResourceLocation(Spacedustry.MOD_ID, getItemName(result) + "_from_blasting_" + getItemName(item)));
     }
 
     @NotNull
--- src/main/java/com/newjumper/spacedustry/datagen/data/recipes/StonecuttingRecipeProvider.java
+++ src/main/java/com/newjumper/spacedustry/datagen/data/recipes/StonecuttingRecipeProvider.java
@@ -1,9 +1,14 @@
 package com.newjumper.spacedustry.datagen.data.recipes;
 
+import com.newjumper.spacedustry.Spacedustry;
 import com.newjumper.spacedustry.block.SpacedustryBlocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.SingleItemRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,8 +20,12 @@ public class StonecuttingRecipeProvider extends RecipeProvider implements ICondi
     }
 
     @Override
-    protected void buildCraftingRecipes(Consumer<FinishedRecipe> consumer) {
-        stonecutterResultFromBase(consumer, SpacedustryBlocks.MOON_STONE_BRICKS.get(), SpacedustryBlocks.COBBLED_MOON_STONE.get());
+    protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> consumer) {
+        resultFromBase(SpacedustryBlocks.MOON_STONE_BRICKS.get(), SpacedustryBlocks.COBBLED_MOON_STONE.get(), consumer);
+    }
+
+    private void resultFromBase(ItemLike result, ItemLike base, Consumer<FinishedRecipe> consumer) {
+        SingleItemRecipeBuilder.stonecutting(Ingredient.of(base), result, 1).unlockedBy(getHasName(base), has(base)).save(consumer, new ResourceLocation(Spacedustry.MOD_ID, getConversionRecipeName(result, base) + "_stonecutting"));
     }
 
     @NotNull
